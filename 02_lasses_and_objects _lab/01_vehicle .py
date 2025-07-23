45449d40bbf43f8afad976e8debb5bb0860ca9e5
wako8o <emilaleksandrov11@gmail.com>
2025-07-23 14:13:20 +0200

    01_vehicle.py

---
+++ 02_lasses_and_objects _lab/01_vehicle .py
@@ -0,0 +1,15 @@
+class Vehicle:
+
+    def __init__(self, mileage: int, max_speed=150) -> None:
+        self.mileage = mileage
+        self.max_speed = max_speed
+        self.gadgets = []   
+
+car = Vehicle(20)
+print(car.max_speed)
+print(car.mileage)
+print(car.gadgets)
+car.gadgets.append('Hudly Wireless')
+print(car.gadgets)
+   
+    
