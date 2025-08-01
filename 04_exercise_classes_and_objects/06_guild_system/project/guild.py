9623c40ff0e34e872ab5ef347108caed8f6233c5
wako8o <emilaleksandrov11@gmail.com>
2025-08-01 23:37:25 +0200

    new

---
+++ 04_exercise_classes_and_objects/06_guild_system/project/guild.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Guild:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.players: List[str] = []
+
+    def assign_player(self, player):
+
+        if player.guild == 'Unaffiliated':
+            player.guild = self.name
+            self.players.append(player)
+            return f"Welcome player {player.name} to the guild {self.name}"
+
+        elif player.guild == self.name:
+            return f"Player {player.name} is already in the guild."
+        else:
+            return f"Player {player.name} is in another guild."
+
+
+    def kick_player(self, player_name: str):
+
+        for p in self.players:
+            if p.name == player_name:
+                self.players.remove(p)
+                p.guild = 'Unaffiliated'
+            return f"Player {p.name} has been removed from the guild."
+
+        return f"Player {player_name} is not in the guild."
+
+    def guild_info(self):
+
+        return f"Guild: {self.name}\n{''.join([player.player_info() for player in self.players])}"
