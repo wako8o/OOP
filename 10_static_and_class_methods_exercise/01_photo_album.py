e27b4fd10e5fc8ebecdae370df4aab9e81f8f44f
wako8o <emilaleksandrov11@gmail.com>
2025-08-19 23:33:22 +0200

    new

---
+++ 10_static_and_class_methods_exercise/01_photo_album.py
@@ -0,0 +1,33 @@
+import math
+
+
+class PhotoAlbum:
+
+    def __init__(self, pages):
+        self.pages = pages
+        self.photos = [[] for _ in range(pages)]
+
+    @classmethod
+    def from_photos_count(cls, photos_count):
+        '''Mathematical rounding when dividing by 4.
+            Takes the upper integer.'''
+        pages = math.ceil(photos_count / 4)
+        return cls(pages)
+
+    def add_photo(self, label: str):
+        for photo in range(len(self.photos)):
+            page = self.photos[photo]
+            if len(page) < 4:
+                page.append(label)
+                return f"{label} photo added successfully on page {photo + 1} slot {len(page)}"
+        return "No more free slots"
+
+    def display(self):
+        result = [11 * '-']
+        for page in self.photos:
+            photo = ' '.join(["[]"] * len(page))
+
+            result.append(photo)
+            result.append(11 * '-')
+        result = '\n'.join(result)
+        return result
