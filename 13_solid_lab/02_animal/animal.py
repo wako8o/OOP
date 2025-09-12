f4e9711de789934ad0814a7d9453eae52d9784e1
wako8o <emilaleksandrov11@gmail.com>
2025-09-12 07:23:50 +0200

    new

---
+++ 13_solid_lab/02_animal/animal.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Cat(Animal):
+
+    def make_sound(self):
+        return 'meow'
+
+class Dog(Animal):
+
+    def make_sound(self):
+        return 'woof-woof'
+
+class Chicken(Animal):
+
+    def make_sound(self):
+        return 'cluck'
+
+def animal_sound(animals: list):
+    for animal in animals:
+
+        print(animal.make_sound())
+
+
+animals = [Cat(), Dog(), Chicken()]
+animal_sound(animals)
