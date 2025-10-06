b12c3054523f573cb0bd0bc8f9fc0207c042ffab
wako8o <emilaleksandrov11@gmail.com>
2025-10-06 21:27:06 +0200

    new

---
+++ 18_exercise_decorators/02_even_parameters.py
@@ -0,0 +1,18 @@
+def even_parameters(function):
+
+    def wrapper(*args, **kwargs):
+        for arg in args:
+            if not isinstance(arg, int) or arg % 2 != 0:
+                return 'Please use only even numbers!'
+        return function(*args, **kwargs)
+    return wrapper
+
+@even_parameters
+def multiply(*nums):
+    result = 1
+    for num in nums:
+        result *= num
+    return result
+
+print(multiply(2, 4, 6, 8))
+print(multiply(2, 4, 9, 8))
